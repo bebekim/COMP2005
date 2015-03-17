a1574d7241d02f674c956fd7d802646f28251b21
Young Ha Kim <yh.kim@outlook.com>
2015-03-17 13:52:03 +1100

    Add exercise 2.4

--- exercise2_4.c
+++ exercise2_4.c
@@ -4,11 +4,9 @@
 int main (int argc, char *argv[]) {
    printf("The maximum value of int is %d\n", INT_MAX);
    printf("The minimum value of int is %d\n", INT_MIN);
-/*
-   printf("The maximum value of float is %f\n", FLT_MAX);
-   printf("The minimum value of float is %f\n", FLT_MIN);
-   printf("The maximum value of double is %f\n", DBL_MAX);
-   printf("The minimum value of double is %f\n", DBL_MIN);
-*/
+   printf("The maximum value of float is %lf\n", FLT_MAX);
+   printf("The minimum value of float is %lf\n", FLT_MIN);
+   printf("The maximum value of double is %lf\n", DBL_MAX);
+   printf("The minimum value of double is %lf\n", DBL_MIN);
    return 0;
 }
