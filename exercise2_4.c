b214c7d4325da2a64ff95d590f06a6d7c9929e75
Young Ha Kim <yh.kim@outlook.com>
2015-03-17 13:24:54 +1100

    limits of c data types

---
+++ exercise2_4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main (int argc, char *argv[]) {
+   printf("The maximum value of int is %d\n", INT_MAX);
+   printf("The minimum value of int is %d\n", INT_MIN);
+/*
+   printf("The maximum value of float is %f\n", FLT_MAX);
+   printf("The minimum value of float is %f\n", FLT_MIN);
+   printf("The maximum value of double is %f\n", DBL_MAX);
+   printf("The minimum value of double is %f\n", DBL_MIN);
+*/
+   return 0;
+}
