b92f381a28f9279ee55faddfd3999895c05efffa
Young Ha Kim <kim.yh@luuv.it>
2015-03-17 14:34:38 +1100

    Update exercise2_4.c

--- exercise2_4.c
+++ exercise2_4.c
@@ -4,9 +4,14 @@
 int main (int argc, char *argv[]) {
    printf("The maximum value of int is %d\n", INT_MAX);
    printf("The minimum value of int is %d\n", INT_MIN);
+   
+   // limits.h no longer contains FLT, DBL constants.
+   /*
    printf("The maximum value of float is %lf\n", FLT_MAX);
    printf("The minimum value of float is %lf\n", FLT_MIN);
    printf("The maximum value of double is %lf\n", DBL_MAX);
    printf("The minimum value of double is %lf\n", DBL_MIN);
+   */
+   
    return 0;
 }
